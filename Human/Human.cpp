e8dfff8951116975280d4b118afcfc1edceaeaaa
Sosn_dy61 <Sosn_dy61@29-7>
2025-09-08 13:17:49 +0300

    iii

--- Human/Human.cpp
+++ Human/Human.cpp
@@ -1,1 +1,49 @@
 #include "Human.h"
+
+Human::Human()
+{
+
+}
+
+Human::Human(const char* n, const char* s, int a)
+{
+}
+
+Human::~Human()
+{
+}
+
+void Human::Input()
+{
+}
+
+void Human::Print()
+{
+}
+
+char* Human::GetName()
+{
+	return nullptr;
+}
+
+char* Human::GetSurname()
+{
+	return nullptr;
+}
+
+int Human::GetAge()
+{
+	return 0;
+}
+
+void Human::SetName(const char* n)
+{
+}
+
+void Human::SetSurname(const char* s)
+{
+}
+
+void Human::SetAge(int a)
+{
+}
--- Human/Human.h
+++ Human/Human.h
@@ -1,5 +1,22 @@
 #pragma once
 class Human
 {
-};
+	char* name;
+	char* surname;
+	int age;
 
+public:
+	Human();
+	Human(const char* n, const char* s, int a);
+	~Human();
+	void Input();
+	void Print();
+
+	char* GetName();
+	char* GetSurname();
+	int GetAge();
+
+	void SetName(const char* n);
+	void SetSurname(const char* s);
+	void SetAge(int a);
+};
